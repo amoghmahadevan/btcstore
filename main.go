00288cd0791bd17ed7423e8c87a645d9d21507aa
amoghmahadevan <amogh.mahadevan1@gmail.com>
2025-06-06 11:30:45 -0700

    stream btc prices with websockets

--- main.go
+++ main.go
@@ -4,13 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/amoghmahadevan/btcstore/config"
 	"github.com/amoghmahadevan/btcstore/handlers"
+	"github.com/amoghmahadevan/btcstore/lib/coinapi"
 )
 
 const LISTEN_ON = ":3000"
 
+var COINAPI_API_KEY = config.MustGetenv("COINAPI_API_KEY")
+
 func main() {
-	r := handlers.Router()
+	wsClient := coinapi.NewCoinAPIWSClient(COINAPI_API_KEY)
+	err := wsClient.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := handlers.Router(wsClient)
 
 	log.Printf("Listening on %s", LISTEN_ON)
 	log.Fatal(http.ListenAndServe(LISTEN_ON, r))
